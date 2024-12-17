ae5428b058148a60d9f6ad3c5fb188749fc0536f
Gabriele Garofalo <gabrielegarofalo999@gmail.com>
2024-12-17 03:06:20 +0100

    Extended Grappling Hook System

--- Source/GVUEDK/Private/Features/Gameplay/GrabPointSystem/GrapplingHookComponent.cpp
+++ Source/GVUEDK/Private/Features/Gameplay/GrabPointSystem/GrapplingHookComponent.cpp
@@ -4,7 +4,6 @@
 #include "GVUEDK/Public/Features/Gameplay/GrapplingHookSystem/GrapplingHookComponent.h"
 
 #include <string>
-
 #include "GameFramework/Character.h"
 #include "Kismet/GameplayStatics.h"
 #include "Utility/FGvDebug.h"
@@ -15,8 +14,18 @@ UGrapplingHookComponent::UGrapplingHookComponent()
 	PrimaryComponentTick.bCanEverTick = true;
 	bTargetAcquired = false;
 	bShowDebug = false;
-	MaxDistance = 0.f;
+	bIsHooking = false;
+	bOrientRotationToMovement = true;
+	MaxDistance = 3000.f;
 	MinDistance = 0.f;
+	LinearSpeed = 2000.f;
+	TotalHookDistance = 0.f;
+	StartDelay = 0.f;
+	ElapsedTime = 0.f;
+	StartHookLocation = FVector::ZeroVector;
+	EndHookLocation = FVector::ZeroVector;
+	StartHookDirection = FVector::ZeroVector;
+	SpeedCurve = nullptr;
 	TargetGrabPoint = nullptr;
 }
 
@@ -33,6 +42,16 @@ void UGrapplingHookComponent::TickComponent(float DeltaTime, ELevelTick TickType
 
 	//FGvDebug::Log(TargetGrabPoint != nullptr ? Cast<AActor>(TargetGrabPoint)->GetName() : "No Target", true);
 	//FGvDebug::Log(std::to_string(InRangeGrabPoints.Num()).c_str(), true);
+	if (bIsHooking)
+	{
+		if (ElapsedTime < StartDelay)
+		{
+			ElapsedTime += DeltaTime;
+			return;
+		}
+		PerformHooking(DeltaTime);
+		return;
+	}
 	
 	// check if there are any grab points in range (MaxCheckDistance)
 	if (LookForGrabPoints(InRangeGrabPoints))
@@ -47,6 +66,57 @@ void UGrapplingHookComponent::TickComponent(float DeltaTime, ELevelTick TickType
 		TargetGrabPoint = nullptr;
 		bTargetAcquired = false;
 	}
+}
+
+void UGrapplingHookComponent::StartHooking()
+{
+	if (!bTargetAcquired || bIsHooking)
+	{
+		return;
+	}
+	ElapsedTime = 0.f;
+	StartHookLocation = GetOwner()->GetActorLocation();
+	EndHookLocation = TargetGrabPoint->GetLandingPoint();
+	TotalHookDistance = FVector::Dist(StartHookLocation, EndHookLocation);
+	StartHookDirection = (EndHookLocation - StartHookLocation).GetSafeNormal();
+	OnStartHooking.Broadcast(StartHookLocation, EndHookLocation, StartHookDirection, TotalHookDistance);
+	if (bOrientRotationToMovement)
+		GetOwner()->SetActorRotation(FRotator(StartHookDirection.Rotation().Roll, StartHookDirection.Rotation().Yaw, 0.f));
+	bIsHooking = true;
+}
+
+void UGrapplingHookComponent::StopHooking()
+{
+	TargetGrabPoint = nullptr;
+	bTargetAcquired = false;
+	bIsHooking = false;
+	OnStopHooking.Broadcast();
+}
+
+void UGrapplingHookComponent::PerformHooking(float DeltaTime)
+{
+	GetOwner()->SetActorLocation(GetOwner()->GetActorLocation() + StartHookDirection * GetSpeed() * DeltaTime/*, false, nullptr, ETeleportType::TeleportPhysics*/);
+	OnHooking.Broadcast();
+	if (GetElapsedNormalizedDistance() >= 1)
+	{
+		GetOwner()->SetActorLocation(EndHookLocation);
+		StopHooking();
+	}
+}
+
+float UGrapplingHookComponent::GetSpeed() const
+{
+	if (!IsValid(SpeedCurve))
+	{
+		return LinearSpeed;
+	}
+	const float CurveInput = SpeedCurve->GetFloatValue(FVector::DistSquared(GetOwner()->GetActorLocation(), StartHookLocation) / (TotalHookDistance * TotalHookDistance)); 
+	return SpeedCurve->GetFloatValue(CurveInput);
+}
+
+FVector UGrapplingHookComponent::GetDirection() const
+{
+	return (EndHookLocation - GetOwner()->GetActorLocation()).GetSafeNormal();
 }
 
 bool UGrapplingHookComponent::LookForGrabPoints(TSet<IGrabPoint*>& OutGrabPoints) const
@@ -95,13 +165,19 @@ IGrabPoint* UGrapplingHookComponent::GetNearestGrabPoint(TSet<IGrabPoint*>& Vali
 	return ReturnValue;
 }
 
+float UGrapplingHookComponent::GetElapsedNormalizedDistance() const
+{
+	return FVector::DistSquared(GetOwner()->GetActorLocation(), StartHookLocation) / (TotalHookDistance * TotalHookDistance);
+}
+
+
 bool UGrapplingHookComponent::PerformSphereTrace(TArray<FHitResult>& HitResults) const
 {
 	const FCollisionShape CollisionShape = FCollisionShape::MakeSphere(MaxDistance);
 	constexpr ECollisionChannel GrabPointsChannel = ECC_GameTraceChannel1;
 
 	
-	bool bHit = GetWorld()->SweepMultiByChannel(
+	const bool bHit = GetWorld()->SweepMultiByChannel(
 		HitResults, 
 		GetOwner()->GetActorLocation(),
 		GetOwner()->GetActorLocation(),
--- Source/GVUEDK/Public/Features/Gameplay/GrapplingHookSystem/GrapplingHookComponent.h
+++ Source/GVUEDK/Public/Features/Gameplay/GrapplingHookSystem/GrapplingHookComponent.h
@@ -6,11 +6,22 @@
 #include "GrabPoint.h"
 #include "GrapplingHookComponent.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_FourParams(FStartHooking, FVector, StartLocation, FVector, EndLocation, FVector, Direction, float, TotalDistance);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FHooking);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FStopHooking);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class GVUEDK_API UGrapplingHookComponent : public UActorComponent
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable)
+	FStartHooking OnStartHooking;
+	UPROPERTY(BlueprintAssignable)
+	FHooking OnHooking;
+	UPROPERTY(BlueprintAssignable)
+	FStopHooking OnStopHooking;
 
 private:
 	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true", ClampMin = "1.0"))
@@ -18,24 +29,59 @@ private:
 	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true", ClampMin = "0.0"))
 	float MinDistance;
 	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
+	UCurveFloat* SpeedCurve;
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true", ToolTip = "Used only if no SpeedCurve provided"))
+	float LinearSpeed;
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true", ClampMin = "0.0"))
+	float StartDelay;
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
+	bool bOrientRotationToMovement;
+	UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = "true"))
 	bool bShowDebug;
 
 	TSet<IGrabPoint*> InRangeGrabPoints;
 	IGrabPoint* TargetGrabPoint;
 
+	FVector StartHookLocation;
+	FVector EndHookLocation;
+	FVector StartHookDirection;
+	float TotalHookDistance;
+	float ElapsedTime;
+	
 	bool bTargetAcquired;
+	bool bIsHooking;
 
 public:
 	UGrapplingHookComponent();
 	virtual void BeginPlay() override;
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
 	                           FActorComponentTickFunction* ThisTickFunction) override;
-	
+
+	bool IsTargetAcquired() const { return bTargetAcquired; }
 	float GetMaxDistance() const { return MaxDistance; }
 	float GetMinDistance() const { return MinDistance; }
+	float GetTotalHookDistance() const { return bIsHooking ? TotalHookDistance : 0.f; }
+	float GetStartDelay() const { return StartDelay; }
+	FVector GetStartLocation() const { return bIsHooking ? StartHookLocation : FVector::ZeroVector; }
+	FVector GetEndLocation() const { return bIsHooking ? EndHookLocation : FVector::ZeroVector; }
+	FVector GetStartDirection() const { return bIsHooking ? StartHookDirection : FVector::ZeroVector; }
+
+	UFUNCTION(BlueprintCallable)
+	void StartHooking();
+	UFUNCTION(BlueprintCallable)
+	void StopHooking();
+
+protected:
+	virtual void PerformHooking(float DeltaTime);
+	virtual float GetSpeed() const;
+	virtual FVector GetDirection() const;
 
 private:
 	bool LookForGrabPoints(TSet<IGrabPoint*>& OutGrabPoints) const;
 	bool PerformSphereTrace(TArray<FHitResult>& HitResults) const;
 	IGrabPoint* GetNearestGrabPoint(TSet<IGrabPoint*>& ValidGrabPoints) const;
+	float GetElapsedNormalizedDistance() const;
+	
 };
+
+
