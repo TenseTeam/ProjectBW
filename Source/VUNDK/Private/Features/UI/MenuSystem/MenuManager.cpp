fa311eb591c9a9ec0622ac44139eeccc0676d452
Denis Faraci <120319520+zVoyder@users.noreply.github.com>
2025-01-09 00:49:05 +0100

    added contextsmanager

--- Source/VUNDK/Private/Features/UI/MenuSystem/MenuManager.cpp
+++ Source/VUNDK/Private/Features/UI/MenuSystem/MenuManager.cpp
@@ -1,36 +1,31 @@
 // Copyright Villains, Inc. All Rights Reserved.
 
 #include "Features/UI/MenuSystem/MenuManager.h"
-#include "Kismet/GameplayStatics.h"
+#include "Features/Generic/IMCManagerSystem/ContextsManagerUtility.h"
 
 void UMenuManager::OpenMenu(UMenuWidget* Menu)
 {
-	if (!Check())
-		return;
-	
 	if (IsValid(CurrentOpenMenu))
 		CloseMenu();
 
 	Menu->Open();
 	CurrentOpenMenu = Menu;
-	SetMenuMappingContext();
+
+	if (bSwitchMappingContexts)
+		UContextsManagerUtility::SetUIMappingContext();
 }
 
 void UMenuManager::CloseMenu()
 {
-	if (!Check())
-		return;
-
 	CurrentOpenMenu->Close();
 	CurrentOpenMenu = nullptr;
-	SetGameplayMappingContext();
+
+	if (bSwitchMappingContexts)
+		UContextsManagerUtility::SetGameplayMappingContext();
 }
 
 void UMenuManager::ToggleMenu(UMenuWidget* Menu)
 {
-	if (!Check())
-		return;
-
 	if (CurrentOpenMenu == Menu)
 	{
 		CloseMenu();
@@ -46,32 +41,4 @@ void UMenuManager::ToggleMenu(UMenuWidget* Menu)
 UMenuWidget* UMenuManager::GetCurrentOpenMenu() const
 {
 	return CurrentOpenMenu;
-}
-
-void UMenuManager::BeginPlay()
-{
-	Super::BeginPlay();
-
-	PlayerController = UGameplayStatics::GetPlayerController(GetWorld(), 0);
-	EnhancedInputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
-
-	if (Check())
-		SetGameplayMappingContext();
-}
-
-void UMenuManager::SetGameplayMappingContext() const
-{
-	EnhancedInputSubsystem->AddMappingContext(GameplayContext, 0);
-	EnhancedInputSubsystem->RemoveMappingContext(MenuContext);
-}
-
-void UMenuManager::SetMenuMappingContext() const
-{
-	EnhancedInputSubsystem->AddMappingContext(MenuContext, 0);
-	EnhancedInputSubsystem->RemoveMappingContext(GameplayContext);
-}
-
-bool UMenuManager::Check() const
-{
-	return IsValid(PlayerController) && IsValid(EnhancedInputSubsystem);
 }
